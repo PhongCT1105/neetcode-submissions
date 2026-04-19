96ece256557293ec4a802e7de788e9a70000fa84
Phong Cao <caothanhphong1105@gmail.com>
2026-04-19 18:36:27 -0400

    Add: web-crawler - submission-0

---
+++ Data Structures & Algorithms/web-crawler/submission-0.py
@@ -0,0 +1,30 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+
+class Solution:
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+
+        def get_host_name(url):
+            return url.split("/")[2]
+
+        visited = set()
+        stack = [startUrl]
+        start_host_name = get_host_name(startUrl)
+
+        while stack:
+            node = stack.pop()
+            if node not in visited and get_host_name(node) == start_host_name:
+                visited.add(node)
+            neighbour = htmlParser.getUrls(node)
+            for i in neighbour:
+                stack.append(i)
+
+        return visited
