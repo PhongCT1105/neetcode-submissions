5f9e8fb2b6d0b183bce0bfaebdb792717f45a620
Phong Cao <caothanhphong1105@gmail.com>
2026-04-23 16:32:37 -0400

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.py
@@ -0,0 +1,33 @@
+class Twitter:
+
+    def __init__(self):
+        from collections import defaultdict
+        self.followee = defaultdict(set)
+        self.tweet = defaultdict(list)
+        self.time = 0
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.time += 1
+        self.tweet[userId].append((self.time, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        all_followee = self.followee[userId].copy()
+        all_followee.add(userId)
+        import heapq
+        heap = []
+        for followee in all_followee:
+            posts = self.tweet[followee]
+            for post in posts:
+                if len(heap) < 10:
+                    heapq.heappush(heap, post)
+                else:
+                    heapq.heappushpop(heap, post)
+        heap.sort(reverse = True)
+        return [tweetId for time, tweetId in heap]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followee[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followee[followerId].discard(followeeId)
