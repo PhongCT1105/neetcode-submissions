932ba88266632a535ab7c1a77600bd9801250132
Phong Cao <caothanhphong1105@gmail.com>
2026-04-25 18:05:55 -0400

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # DFS recursive
+        res = 0
+        def post_order(root):
+            nonlocal res            
+            if not root:
+                return 0
+            left = in_order(root.left)
+            right = in_order(root.right)
+            total = left + right
+            res = max(res, total)
+
+            return 1 + max(left, right)
+        
+        post_order(root)
+        return res
