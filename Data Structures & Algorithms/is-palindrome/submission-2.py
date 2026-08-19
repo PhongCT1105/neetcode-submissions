6d7f3be7fb4e449df498495f1ae40a087b68146b
Phong Cao <caothanhphong1105@gmail.com>
2026-08-18 22:57:51 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        def is_char(c):
+            if ord('a') <= ord(c) <= ord('z'):
+                return True
+            if ord('A') <= ord(c) <= ord('Z'):
+                return True
+            return False
+
+        l, r = 0, len(s)-1
+        while l <= r:
+            print(s[l], s[r])
+            if not is_char(s[l]):
+                l += 1
+                continue
+            if not is_char(s[r]):
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1  
+            r -= 1
+
+        return True
